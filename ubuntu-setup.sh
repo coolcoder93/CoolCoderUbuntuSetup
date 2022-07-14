a9a20b19ff5fbfa11c95d8fddb105f91f6ff6b80
CoolCoder93 <96700307+coolcoder93@users.noreply.github.com>
2022-07-14 18:37:31 +0200

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apt update && apt upgrade -y
+snap refresh
 # SDL2
 apt install libsdl2-2.0-0 libsdl2-dev libsdl2-gfx-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev -y
 # GLFW, GLEW, GLM
