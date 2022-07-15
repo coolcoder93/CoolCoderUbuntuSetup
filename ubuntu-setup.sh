00dad21633568e5a5252f47f06ee5c19c96da91b
CoolCoder93 <96700307+coolcoder93@users.noreply.github.com>
2022-07-15 10:00:49 +0200

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -10,7 +10,7 @@ apt install libglfw3-wayland libglfw3-dev libglew-dev libglm-dev -y
 # Python
 apt install python3-dev python3-pip python3-venv -y
 sudo -u $SUDO_USER pip install django
-sudo -u $SUDO_USER echo 'export PATH="/home/simon/.local/bin:$PATH"' | sudo -u $SUDO_USER tee -a ~/.bashrc
+echo 'export PATH="/home/simon/.local/bin:$PATH"' >> /home/$SUDO_USER/.bashrc
 # C/C++
 apt install cmake clang clangd libclang-dev llvm llvm-dev build-essential git -y
 # Nodejs
@@ -33,7 +33,7 @@ gem install solargraph bundler rails
 # PHP/Laravel
 apt install php php-curl php-pear php-gd php-dev php-zip php-mbstring php-mysql php-xml composer -y
 sudo -u $SUDO_USER composer global require laravel/installer
-sudo -u $SUDO_USER echo 'export PATH="~/.composer/vendor/bin:$PATH"' | sudo -u $SUDO_USER tee -a ~/.bashrc
+echo 'export PATH="~/.config/composer/vendor/bin:$PATH"' >> /home/$SUDO_USER/.bashrc
 # Java
 apt install default-jdk openjdk-17-jdk ant maven maven-cache-cleanup -y
 snap install netbeans --classic
@@ -42,19 +42,20 @@ snap install gradle --classic
 apt install torbrowser-launcher gnome-tweaks vim emacs htop neofetch steam gimp codeblocks vlc -y
 snap install bitwarden
 #Emacs setup
-sudo -u $SUDO_USER rm ~/.emacs
-sudo -u $SUDO_USER git clone https://github.com/coolcoder93/CoolCoderEmacsSetup.git
+rm /home/$SUDO_USER/.emacs
+mkdir /home/$SUDO_USER/.emacs.d/
+repository="https://github.com/coolcoder93/CoolCoderEmacsSetup.git"
+sudo -u $SUDO_USER  git clone $repository
 cd CoolCoderEmacsSetup
-sudo -u $SUDO_USER mv init.el ~/.emacs.d/
+mv init.el /home/$SUDO_USER/.emacs.d/
 cd .. && rm -rf CoolCoderEmacsSetup
-sudo -u $SUDO_USER touch ~/.emacs.d/custom.el
-sudo -u $SUDO_USER systemctl --user enable --now emacs
-sudo -u $SUDO_USER echo "alias emacs=\"emacsclient -c -a 'emacs'\" " | sudo -u $SUDO_USER tee -a ~/.bashrc
-sudo -u $SUDO_USER echo "alias emacsterm=\"emacsclient -nw\"" | sudo -u $SUDO_USER tee -a ~/.bashrc
-sudo -u $SUDO_USER echo 'export PATH="/usr/include:$PATH"' | sudo -u $SUDO_USER tee -a ~/.bashrc
+systemctl --machine=$SUDO_USER@.host --user enable --now emacs
+echo "alias emacs=\"emacsclient -c -a 'emacs'\" " >> /home/$SUDO_USER/.bashrc
+echo "alias emacsterm=\"emacsclient -nw\"" >> /home/$SUDO_USER/.bashrc
+echo 'export PATH="/usr/include:$PATH"' >> /home/$SUDO_USER/.bashrc
 # TLP
 apt install tlp tlp-rdw -y
 systemctl enable --now tlp
 # Remove unused programs
 snap remove snap-store 
-apt autoremove
+apt autoremove -y
